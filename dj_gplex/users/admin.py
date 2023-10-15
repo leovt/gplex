37367122c46ec98d2da1133c9ab7a53ed9d64cc3
leovt <leonhard.vogt@gmx.ch>
2023-10-15 21:39:17 +0200

    friendship model

--- dj_gplex/users/admin.py
+++ dj_gplex/users/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Profile, Relationship
 
-# Register your models here.
+class ProfileAdmin(admin.ModelAdmin):
+    pass
+
+class RelationshipAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Profile)
+admin.site.register(Relationship)
--- dj_gplex/users/models.py
+++ dj_gplex/users/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+
+    def __str__(self):
+        return self.user.username
+
+    def friends(self):
+        return [rel.peer for rel in self.relationships.filter(status=Relationship.RelationshipStatus.FRIEND).prefetch_related('peer')]
+
+class Relationship(models.Model):
+    class Meta:
+        unique_together = ('user', 'peer')
+
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='relationships')
+    peer = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='rev_relationships')
+
+    class RelationshipStatus(models.TextChoices):
+        BLOCKED = "B", "Blocked"
+        FRIEND = "F", "Friend"
+
+    status = models.CharField(
+        max_length=2,
+        choices=RelationshipStatus.choices,
+    )
+
+    def __str__(self):
+        return f'{self.user} - {self.peer}: {self.status}'
